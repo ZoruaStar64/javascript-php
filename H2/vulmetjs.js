5af68d0e79a68bdf402617c03cc2d40b39acc259
ZoruaStar64 <sanderb119@gmail.com>
2020-11-19 11:14:46 +0100

    did loads of stuff in H2

---
+++ H2/vulmetjs.js
@@ -0,0 +1,7 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+
+document.getElementById("purpleMario").src = "img/64Zorua.png";
+
+document.getElementById("actualPurpleMario").src = "img/Wah.jpg";
