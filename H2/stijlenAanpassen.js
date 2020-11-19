cb5b3f426b1d0fd1a78d8b8e3e106686ba8bfe94
ZoruaStar64 <sanderb119@gmail.com>
2020-11-19 13:51:51 +0100

    struggling with box

---
+++ H2/stijlenAanpassen.js
@@ -0,0 +1,12 @@
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl1").style.marginTop = "0px";
+
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "10px";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "30px";
+
+document.getElementById("bl4").style.backgroundColor = "red";
+document.getElementById("bl4").style.marginTop = "0px";
