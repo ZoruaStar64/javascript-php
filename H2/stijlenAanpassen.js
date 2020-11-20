32988594b5cfa7d2f4ae0456a0cec23de05b4bc3
ZoruaStar64 <sanderb119@gmail.com>
2020-11-20 10:42:51 +0100

    Finished H2

--- H2/stijlenAanpassen.js
+++ H2/stijlenAanpassen.js
@@ -1,12 +1,21 @@
 document.getElementById("bl1").style.backgroundColor = "cyan";
 document.getElementById("bl1").style.marginTop = "0px";
-
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl1").style.paddingTop = "10px";
+document.getElementById("bl1").style.fontFamily = "Saira Extra Condensed";
 
 document.getElementById("bl2").style.backgroundColor = "green";
 document.getElementById("bl2").style.marginTop = "10px";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl2").style.fontFamily = "Saira Extra Condensed";
 
 document.getElementById("bl3").style.backgroundColor = "yellow";
-document.getElementById("bl3").style.marginTop = "30px";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl3").style.fontFamily = "Saira Extra Condensed";
 
 document.getElementById("bl4").style.backgroundColor = "red";
 document.getElementById("bl4").style.marginTop = "0px";
+document.getElementById("bl4").style.textAlign = "left";
+document.getElementById("bl4").style.fontFamily = "Saira Extra Condensed";
+
