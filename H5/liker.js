b3ad49f2e468f9b3f770a2d98fdfb1e0aadcc507
ZoruaStar64 <sanderb119@gmail.com>
2020-11-30 16:52:32 +0100

    done with H5

--- H5/liker.js
+++ H5/liker.js
@@ -1,22 +1,47 @@
+
+
 gallery = document.getElementById("gallery");
 
-creategalleryHolders();
+createGalleryHolders();
 createImages();
 
-function creategalleryHolders() {
+function createGalleryHolders() {
     for (var i = 0; i < 9; i++) {
-        pictureHolder = document.createElement("div")
-        pictureHolder.className = "picture";
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "picture-holder";
         pictureHolder.id = "picture-holder-" + i;
         gallery.appendChild(pictureHolder);
+
     }
 }
 
 function createImages() {
-    pictureHolder = document.getElementsByClassName("picture");
+    pictureHolder = document.getElementsByClassName("picture-holder");
+
     for (var i = 0; i < pictureHolder.length; i++) {
+        favorite = document.createElement("div");
+        favorite.className = "favorite";
+        favorite.id = "favorite_" + (i+1);
       charImage = document.createElement("img");
-      charImage.src = "../img/Like" + (i+1) + ".png"
+      charImage.src = "../img/Like" + (i+1) + ".png";
+
+      charImage.id = (i+1);
+      charImage.addEventListener("click", function () {
+          likeIt(this.id);
+        });
+      pictureHolder[i].appendChild(favorite);
+      pictureHolder[i].appendChild(charImage);
 
     }
+}
+
+function likeIt(id) {
+ favorite = document.getElementById("favorite_" + id);
+ notfavorite = document.getElementsByClassName("favorite");
+
+for (var i = 0; i < notfavorite.length; i++) {
+    notfavorite[i].style.backgroundImage = "none";
+}
+favorite.style.backgroundImage = "url(../img/heart.jpg)";
+favorite.style.backgroundSize = "300px 150px";
 }
