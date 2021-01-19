83001fe9eb4ba88406d3e67bb795890c237bb732
Zorua <sanderb119@gmail.com>
2021-01-19 14:52:34 +0100

    edited the info button

--- H6/Yathzee.js
+++ H6/Yathzee.js
@@ -8,7 +8,20 @@ let totalPointsPlayer2 = 0;
 let players = ["Player 1", "Player 2"];
 let currentPlayer = Math.floor(Math.random() * 2);
 
-let keepDice = function() {
-   if (this.id) {removeEventListener('click', rollsLeft)
-   }
-}
+// let keepDice = function() {
+//    if (this.id) {removeEventListener('click', rollsLeft)
+//    }
+// };
+
+let rollDices = function() {
+    let rollButton = document.getElementsByClassName("rollButton");
+    if (rollsLeft > 0 ) {
+
+    }
+};
+
+function resetVariables() {
+    waitingForNextMove = false;
+    let nextButton = document.getElementsByClassName("nextButton");
+    nextButton.addEventListener('click', );
+};
