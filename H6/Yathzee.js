d5276c01cd47ea01eacc4a3cf58e0c0d88900615
Zorua <sanderb119@gmail.com>
2021-01-19 11:44:45 +0100

    edited positions and made 2 new buttons

---
+++ H6/Yathzee.js
@@ -0,0 +1,14 @@
+let winner = null;
+let rollsLeft = 3;
+let diceNumbers = ["", "", "", "", ""];
+let dicesClicked = ["1", "2", "3", "4", "5"];
+let waitingForNextMove = true;
+let totalPointsPlayer1 = 0;
+let totalPointsPlayer2 = 0;
+let players = ["Player 1", "Player 2"];
+let currentPlayer = Math.floor(Math.random() * 2);
+
+let keepDice = function() {
+   if (this.id) {removeEventListener('click', rollsLeft)
+   }
+}
